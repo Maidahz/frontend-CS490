d5c11a82d5ce47f157b94e6c095bcddef849e9ea
maidah <maidahhaseeba@gmail.com>
2023-10-12 17:54:37 -0400

    TopMoviesList editted

--- src/components/TopMovies/TopMoviesList.jsx
+++ src/components/TopMovies/TopMoviesList.jsx
@@ -1,53 +1,9 @@
-// import React from 'react';
-// import axios from 'axios';
-// import { Link } from 'react-router-dom';
-// import './Top-Movies.css'
-
-// class TopMoviesList extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         movielist: null,
-//         error: null, //error statement
-//       };
-//     }
-//     // loads and renders data after component is displayed
-//     componentDidMount() {
-//       axios.get('http://localhost:5000/top-rented-movies')
-//       .then(response => {
-//         this.setState({ movielist: response.data });
-//       })
-//       .catch (error => {
-//         console.log("Error: fetching top movies.", error);
-//         this.setState({ error: 'Error fetching top movies.' }); // Set error state
-//       });
-//     }
-
-//   render() {
-//     const { movielist, error} = this.state; // Correct variable name, destructured from state
-//     //console.log(movielist);
-//     return (
-//         <div className='.list-container'>
-//             <h1>Top 5 Rented Movies</h1>
-//             {error && <p>{error}</p>} {/* Display error message if there is an error */}
-//             <ul>
-//                 {movielist ? movielist.map(movie => ( // Corrected here
-//                     <li key={movie.film_title}>
-//                         <Link to={`/movie/${movie.film_title}`}>{movie.film_title}</Link>
-//                     </li>
-//                 )) : <p>Loading...</p>}
-//             </ul>
-//         </div>
-//     );
-//   }
-// }
-
-// export default TopMoviesList;
-
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
 import './Top-Movies.css';
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 class TopMoviesList extends React.Component {
   constructor(props) {
@@ -55,6 +11,7 @@ class TopMoviesList extends React.Component {
     this.state = {
       movielist: null,
       error: null,
+      selectedMovie: null,
     };
   }
 
@@ -72,11 +29,11 @@ class TopMoviesList extends React.Component {
 
   handleMovieClick = (movieId) => {
     // Call a function passed from App.jsx to handle the movie click
-    this.props.onMovieClick(movieId);
+    this.setState({ selectedMovie: movieId });
   };
 
   render() {
-    const { movielist, error } = this.state;
+    const { movielist, error, selectedMovie } = this.state;
 
     return (
       <div className="top-movies">
@@ -86,16 +43,21 @@ class TopMoviesList extends React.Component {
           {movielist
             ? movielist.slice(0, 5).map((movie) => (
                 <li key={movie.film_title}>
-                  <Link
+                  {/* <Link
                     to={`/homepage/movie/${movie.film_title}`}
                     onClick={() => this.handleMovieClick(movie.id)} // Pass movie id on click
+                  > */}
+                  <span
+                    className='movie-name'
+                    onClick={() => this.handleMovieClick(movie.id)}  // Use a span with an onClick event to handle the click
                   >
                     {movie.film_title}
-                  </Link>
+                  </span>
                 </li>
               ))
             : <p>Loading...</p>}
         </ul>
+        <MovieDetails id={selectedMovie} />  {/* Render the MovieDetails component and pass the selected movie's id to it */}
       </div>
     );
   }
